73f5d0281a533c51bb4c042eccbe333a88b546ed
Armando Duran <luisarmandodv@gmail.com>
2019-02-09 20:42:54 -0600

    Initial Commit

---
+++ from_iOS_to_RN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  from_iOS_to_RN
+//
+//  Created by Armando-MBP-463 on 2/9/19.
+//  Copyright © 2019 luisarmandodv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
